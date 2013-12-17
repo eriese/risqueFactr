8d29031e6c92519143ed727209797eefca65eba0
Enoch Riese <enoch.riese@gmail.com>
2013-12-17 15:44:15 -0500

    updated seeds for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-Instrument.delete_all
-Pronoun.delete_all
-Disease.delete_all
+
 instruments = Instrument.create([{part: "hand"}, {part: "genitals"}, {part: "anus"}, {part: "mouth"}, {part: "toys"}])
 pronouns = Pronoun.create([{subject: "ze", object: "hir", possessive: "hir" , obj_possessive: "hirs", reflexive: "hirself"}, {subject: "she", object: "her", possessive: "her", obj_possessive: "hers", reflexive: "herself"}, {subject: "he", object: "him", possessive: "his", obj_possessive: "his", reflexive: "himself"}, {subject: "they", object: "them", possessive: "their", obj_possessive: "theirs", reflexive: "themself"}])
-diseases = Disease.create([{name: "chlamydia", gestation_min: 1, gestation_max: 4 }, {name: "gonorrhea", gestation_min: 1, gestation_max: 3 }, {name: "syphillis", gestation_min: 1,gestation_max: 12}, {name: "HIV", gestation_min: 4, gestation_max: 12}, {name: "hepatitis C", gestation_min: 6, gestation_max: 12}, {name: "hepatitis B", gestation_min: 6, gestation_max: 12}, {name: "herpes", gestation_min: 3, gestation_max: 12}, {name: "hepatitis A", gestation_min: 2, gestation_max: 7}, {name: "HPV", gestation_min: 8, gestation_max: 80}, {name: "trichomoniasis", gestation_max: 4, gestation_min: 1}, {name: "cytomegalovirus", gestation_max: 12, gestation_min: 3}, {name: "molluscum contagiosum", gestation_min: 2, gestation_max: 24}, {name: "bacterial vaginosis", gestation_min: 0, gestation_max: 1}])
+diseases = Disease.create([{name: "Chlamydia", gestation_min: 1, gestation_max: 4 }, {name: "Gonorrhea", gestation_min: 1, gestation_max: 3 }, {name: "Syphillis", gestation_min: 1,gestation_max: 12}, {name: "HIV", gestation_min: 4, gestation_max: 12}, {name: "Hepatitis C", gestation_min: 6, gestation_max: 12}, {name: "Hepatitis B", gestation_min: 6, gestation_max: 12}, {name: "Herpes", gestation_min: 3, gestation_max: 12}, {name: "Hepatitis A", gestation_min: 2, gestation_max: 7}, {name: "HPV", gestation_min: 8, gestation_max: 80}, {name: "Trichomoniasis", gestation_max: 4, gestation_min: 1}, {name: "Cytomegalovirus", gestation_max: 12, gestation_min: 3}, {name: "Molluscum Contagiosum", gestation_min: 2, gestation_max: 24}, {name: "Bacterial Vaginosis", gestation_min: 0, gestation_max: 1}])
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
