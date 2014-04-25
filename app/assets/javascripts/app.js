1b8546f56f2284b993036a1416fa3822348e3f46
Enoch Riese <enoch.riese@gmail.com>
2014-04-25 16:37:26 -0400

    js resize

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,14 @@
 setSize = function() {
   if($("body").width() < 750) {
     $("body").addClass("mobile");
+    var fontSize = $("body").width() / 30;
+    if(fontSize > 14) {
+      $(".nav li").css("font-size", fontSize + "px");
+    }
   }
   else {
     $("body").removeClass("mobile");
   }
 }
+$(document).ready(setSize);
 $(window).resize(setSize);
