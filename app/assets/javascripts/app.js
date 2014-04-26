467c9c59c82b6dc1ce97eb72f17e0642915a988d
Enoch Riese <enoch.riese@gmail.com>
2014-04-25 23:16:39 -0400

    cleaner javascript

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,23 @@
 setSize = function() {
-  if($("body").width() < 750) {
+  console.log($(window).width());
+  if($(window).width() < 750) {
     $("body").addClass("mobile");
-    var fontSize = $("body").width() / 30;
-    if(fontSize > 14) {
-      $(".nav li").css("font-size", fontSize + "px");
+    var fontSize;
+    if($(window).width() / 30 > 16) {
+      fontSize = $(window).width() / 30;
     }
+    else {
+      fontSize = 16;
+    }
+    var marginWidth = $(window).width() /40;
+    console.log(fontSize);
+    $(".nav li").css("font-size", fontSize + "px");
+    $(".nav li").css("margin-right", marginWidth + "px");
+    $(".nav ul").css("width", "80%");
   }
   else {
     $("body").removeClass("mobile");
+    $(".nav ul").css("width", "60%");
   }
 }
 $(document).ready(setSize);
