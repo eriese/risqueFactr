142ba99aa7edf76ce8f9c0937b8f3b10b3ea1191
Enoch Riese <enoch.riese@gmail.com>
2014-05-11 03:15:57 -0400

    removed old pre-compiles, moved media query to css

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,20 +1,17 @@
 setSize = function() {
-  if($(window).width() < 1000) {
     var fontSize;
-    if($(window).width() / 50 > 16) {
-      fontSize = $(window).width() / 30;
+    if($(window).width() / 40 > 16) {
+      fontSize = $(window).width() / 40;
     }
     else {
       fontSize = 16;
     }
-    var marginWidth = $(window).width() / 30;
+    if (fontSize > 25) {
+      fontSize = 25;
+    }
+    var marginWidth = $(window).width() / 55;
     $(".nav li").css("font-size", fontSize);
     $(".nav li").css("margin-right", marginWidth);
-  }
-  else {
-    // $("body").removeClass("mobile");
-    // $(".to-hide").addClass("hidden");
-  }
 }
 $(document).ready(setSize);
 $(window).resize(setSize);
